ecd679e120409e242e83d73b119074a370ff3724
lspieth <18659@my4county.net>
2021-01-13 14:47:33 -0500

    Added the Ninety Nine Bottles test.

---
+++ ninetyNineBottles.js
@@ -0,0 +1,7 @@
+for(var bottles = 99; bottles > 0; bottles--)
+{
+	document.write(bottles + "<br>");
+	console.log(bottles + "<br>");
+}
+document.write("No more bottles!");
+console.log("No more bottles!");
