0e963efb471ea4e232f7dfdf6513512bb41f3f76
lspieth <18659@my4county.net>
2021-01-15 14:32:46 -0500

    Completed the 99 Bottles test.

--- ninetyNineBottles.js
+++ ninetyNineBottles.js
@@ -1,7 +1,15 @@
 for(var bottles = 99; bottles > 0; bottles--)
 {
-	document.write(bottles + "<br>");
-	console.log(bottles + "<br>");
+	if (bottles != 1)
+	{
+		document.write(bottles + " bottles of beer on the wall, " + bottles + " bottles of beer!<br>");
+		console.log(bottles + " bottles of beer on the wall, " + bottles + " bottles of beer!<br>");
+ 	}
+	else
+	{
+		document.write(bottles + " bottle of beer on the wall, " + bottles + " bottle of beer!<br>");
+		console.log(bottles + " bottle of beer on the wall, " + bottles + " bottle of beer!<br>");
+ 	}
 }
-document.write("No more bottles!");
-console.log("No more bottles!");
+document.write("No more bottles of beer on the wall!");
+console.log("No more bottles of beer on the wall!!");
