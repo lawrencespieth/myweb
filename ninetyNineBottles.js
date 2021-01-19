5d624068218342e2d7d95b4bf5d40807995a519e
lspieth <18659@my4county.net>
2021-01-19 12:50:55 -0500

    Changed the 99 Bottles test to say 'pop'.

--- ninetyNineBottles.js
+++ ninetyNineBottles.js
@@ -2,14 +2,14 @@ for(var bottles = 99; bottles > 0; bottles--)
 {
 	if (bottles != 1)
 	{
-		document.write(bottles + " bottles of beer on the wall, " + bottles + " bottles of beer!<br>");
-		console.log(bottles + " bottles of beer on the wall, " + bottles + " bottles of beer!<br>");
+		document.write(bottles + " bottles of pop on the wall, " + bottles + " bottles of pop!<br>");
+		console.log(bottles + " bottles of beer on the wall, " + bottles + " bottles of pop!<br>");
  	}
 	else
 	{
-		document.write(bottles + " bottle of beer on the wall, " + bottles + " bottle of beer!<br>");
-		console.log(bottles + " bottle of beer on the wall, " + bottles + " bottle of beer!<br>");
+		document.write(bottles + " bottle of pop on the wall, " + bottles + " bottle of pop!<br>");
+		console.log(bottles + " bottle of pop on the wall, " + bottles + " bottle of pop!<br>");
  	}
 }
-document.write("No more bottles of beer on the wall!");
-console.log("No more bottles of beer on the wall!!");
+document.write("No more bottles of pop on the wall!");
+console.log("No more bottles of pop on the wall!!");
