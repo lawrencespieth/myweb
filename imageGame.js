2cbbcf48b2003a1e6c25f78093b5158e9ca905bd
lspieth <18659@my4county.net>
2021-04-06 14:18:01 -0400

    Added the Image Blurring game

---
+++ imageGame.js
@@ -0,0 +1,26 @@
+window.onload = init;
+
+function init()
+{
+	var images = document.getElementsByTagName("img");
+	for (var i = 0; i < images.length; i++)
+	{
+		images[i].onclick = showAnswer;
+	}
+}
+
+function showAnswer(eventObj)
+{
+	var image = eventObj.target;
+	var name = image.id;
+	name = "images/" + name + ".jpg";
+	image.src = name;
+	setTimeout(reblur, 2000, image);
+}
+
+function reblur(image)
+{
+	var name = image.id;
+	name = "images/" + name + "blur.jpg";
+	image.src = name;
+}
